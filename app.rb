67c1a48fe29e929fcf7b9925fe40631c90330a62
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-04-16 06:01:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,11 +33,13 @@ class App < Sinatra::Base
     @number1 = params[:number1]
     @number2 = params[:number2]
   if @operation = "add"
-    "#{number1} + #{number2}"
+    "#{@number1 + @number2}"
   elsif @operation = "subtract"
-    "#{number1} - #{number2}"
+    "#{@number1 - @number2}"
   elsif @operation = "divide"
+      "#{@number1 / @number2}"
   elsif @operation = "multiply"
+      "#{@number1 - @number2}"
   
   
 end
