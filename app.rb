f1587bd3cb49884f81fb595c8439385f45810c27
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-04-16 05:10:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/reversename/:name' do
-    @name = params[:name]
-    "#{name}".reverse
+    @name = params[:name].reverse
+    #must be reversed on the attribute rather than the returned string
+    @name
+
   end 
   
 end
