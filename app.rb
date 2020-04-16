bb0383805c6fa64a5faa4f2e91a36450a90c46ff
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-04-16 05:51:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
     "#{@num ** 2 }"
   end
   
-  get '/say/:number/phrase' do
-    @number = params[:number]
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
     @phrase = params[:phrase]
     "#{number} + #{phrase}"
   end
